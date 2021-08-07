182078bf25ab0e896b2ab2a05154500c19260739
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-07 10:49:34 -0400

    Add files via upload

---
+++ main (64).py
@@ -0,0 +1,168 @@
+kilometers=float(input('How many kilometers?:'))
+conv_fac=0.621371
+miles=kilometers*conv_fac
+    print('%0.3f kilometers is equal to %0.3f miles' %(kilometers,miles))
+
+
+celsius=float(input('Enter temperature in Celsius: '))
+fahrenheit=(celsius * 1.8)+32
+    print('%0.1f Celsius is equal to %0.1f degree Fahrenheit'%(celsius, fahrenheit))
+
+feet=float(input('Enter measurement in feet: '))
+meters=feet*0.3048
+    print('%0.1f feet is equal to %0.1f meters'%(feet, meters))
+
+milliliters=float(input('Enter amount in milliliters: '))
+gallons=milliliters*0.00026417
+    print('%0.1f milliliters is equal to %0.1f gallons'%(milliliters,gallons))
+
+cubicFeet=float(input('Enter amount in cubic feet: '))
+liters=cubicFeet*28.316
+    print('%0.1f liters is equal to %0.1f feet'%(cubicFeet,liters)
+    
+lightYear=float(input('Enter amount in light years: '))
+feet2=lightYear*9460000000000000
+    print('%0.1f light years is equal to %0.1f feet'%(lightYear,feet2)
+
+pounds=float(input('Enter amount in pounds: '))
+kilograms=pounds*2.20462262
+    print('%0.1f pounds is equal to %0.1f kilograms'%(kilograms, pounds)
+
+mm=float(input('Enter amount in millimeter: '))
+mm=inch*0.03937008
+    print('%0.1f millimeter is equal to %0.1f inches'%(mm, inch)
+
+cm=float(input('Enter amount in centimeters: '))
+cm=inch*0.39370079
+print('%0.1f centimeters is equal to %0.1f inches'%(cm, inch)
+
+cm=float(input('Enter amount in centimeters: '))
+cm=inch*0.39370079
+    print('%0.1f centimeters is equal to %0.1f inches'%(cm, inch)
+
+mile=float(input('Enter amount in miles: '))
+mile=foot*5280
+    print('%0.1f miles is equal to %0.1f feet'%(mile,foot)
+
+mr=float(input('Enter amount in microns: '))
+mr=foot*0.00000328
+    print('%0.1f microns is equal to %0.1f feet'%(mr,foot)
+
+yards=float(input('Enter measurement in yards: '))
+meters=yards*1.0936133
+    print('%0.1f feet is equal to %0.1f meters'%(yards, meters))
+
+ms=float(input('Enter measurement in meters per second: '))
+mph=ms*2.23693629
+    print('%0.1f ms is equal to %0.1f mph'%(ms, mph))
+
+mach=float(input('Enter measurement in mach: '))
+mph=mach*767.269148
+    print('%0.1f mach is equal to %0.1f mph'%(mach, mph))
+
+mm2=float(input('Enter measurement in millimeters: '))
+meter2=mm2*0.001
+    print('%0.1f millimeters is equal to %0.1f meters'%(mm2,meter2))
+
+kph=float(input('Enter measurement in kilometers per hour: '))
+mph=kph*0.62137119
+    print('%0.1f kph is equal to %0.1f mph'%(kph, mph))
+
+pascal=float(input('Enter measurement in pascals: '))
+bar=pascals*0.00001
+    print('%0.1f pascals is equal to %0.1f bar'%(pascals,bar))
+
+Newton=float(input('Enter measurement in newtons: '))
+poundforce=Newtons*0.22480894
+    print('%0.1f Newtons is equal to %0.1f pound-forces'%(Newton,poundforce))
+
+sqfeet=float(input('Enter measurement in square feet: '))
+sqmeters=sqfeet*0.09290304
+    print('%0.1f square feet is equal to %0.1f square meters'%(sqfeet, sqmeters))
+
+Newton=float(input('Enter measurement in newtons: '))
+dyne=Newtons*100000
+    print('%0.1f Newtons is equal to %0.1f dynes'%(Newton,dyne))
+
+Newton=float(input('Enter measurement in newtons: '))
+mtf=Newtons*0.00010197
+    print('%0.1f Newtons is equal to %0.1f metric-ton-force'%(Newton,mtf))
+
+joule=float(input('Enter measurement in joules: '))
+calorie=joule*0.23900574
+    print('%0.1f joules is equal to %0.1f calories'%(joule,calorie))
+
+volt=float(input('Enter measurement in volt: '))
+microvolt=volt*1000000
+    print('%0.1f volts is equal to %0.1f microvolts'%(volt,microvolt))
+
+volt=float(input('Enter measurement in volt: '))
+microvolt=volt*1000000
+    print('%0.1f volts is equal to %0.1f microvolts'%(volt,microvolt))
+
+byte=float(input('Enter measurement in byte: '))
+bit=byte*8
+    print('%0.1f bytes is equal to %0.1f bits'%(byte,bit))
+
+mpg=float(input('Enter measurement in MPG: '))
+kgl=mpg*0.42514371
+    print('%0.1f mpg is equal to %0.1f kgl'%(mpg,kgl))
+
+ohm=float(input('Enter measurement in ohms: '))
+kohm=ohm*0.001
+    print('%0.1f ohms is equal to %0.1f kilo ohms'%(ohm,kohm))
+
+day=float(input('Enter measurement in days: '))
+minute=day*1440
+    print('%0.1f days is equal to %0.1f minutes'%(day,minute))
+
+rev=float(input('Enter measurement in revolutions: '))
+arc=rev*1296000
+    print('%0.1f revolutions is equal to %0.1f arc seconds'%(rev,arc))
+
+rev=float(input('Enter measurement in revolutions: '))
+arcm=rev*21600
+    print('%0.1f revolutions is equal to %0.1f arc minutes'%(rev,arcm))
+
+rev=float(input('Enter measurement in revolutions: '))
+arcm=rev*21600
+    print('%0.1f revolutions is equal to %0.1f arc minutes'%(rev,arcm))
+
+celsius=float(input('Enter temperature in Celsius: '))
+kelvin=celcius+273.15
+    print('%0.1f Celsius is equal to %0.1f Kelvin'%(celsius, kelvin))
+    
+furlong=float(input('Enter measurement in furlongs: '))
+meters=furlong*201.168
+    print('%0.1f furlongs is equal to %0.1f meters'%(furlong, meters))
+    
+    
+rod=float(input('Enter measurement in rods: '))
+meters=rod*5.0292
+    print('%0.1f rods is equal to %0.1f meters'%(rod, meters)) 
+    
+cubit=float(input('Enter measurement in cubits: '))
+meters=cubit*0.4572
+    print('%0.1f cubits is equal to %0.1f meters'%(cubit, meters))
+    
+week=float(input('Enter measurement in week: '))
+hour=week*168
+    print('%0.1f weejs is equal to %0.1f hours'%(week,hour))
+    
+hp=float(input('Enter measurement in horsepower: '))
+watt=hp*745.699872
+    print('%0.1f horsepower is equal to %0.1f watts'%(hp,watt))
+    
+    
+hh=float(input('Enter measurement in hogshead: '))
+cup=hh*1008
+    print('%0.1f hogshead is equal to %0.1f cups'%(hh,cups))
+    
+ton=float(input('Enter measurement in long tons: '))
+ounce=ton*35840
+    print('%0.1f long tons is equal to %0.1f ounce'%(ton,ounce))
+    
+mb=float(input('Enter measurement in megabytes: '))
+bit2=mb*8000000
+    print('%0.1f megabytes is equal to %0.1f bits'%(mb,bit2))
+    
